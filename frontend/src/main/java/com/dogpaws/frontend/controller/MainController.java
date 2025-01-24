916192238e1960ad08cc95295edc08b314a60d92
JeyeonAhn <irem2493@naver.com>
2025-01-24 11:52:34 +0900

    Initial commit 수정된 메시지

---
+++ frontend/src/main/java/com/dogpaws/frontend/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.dogpaws.frontend.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
+
