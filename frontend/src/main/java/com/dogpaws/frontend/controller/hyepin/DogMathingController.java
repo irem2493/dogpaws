f8c1c8a147e2a9374b17993500702163dd6c8c31
DJDJ <jeonguijin121@gmail.com>
2025-02-05 10:41:30 +0900

    캘린더 복구 완료..

--- frontend/src/main/java/com/dogpaws/frontend/controller/hyepin/DogMathingController.java
+++ frontend/src/main/java/com/dogpaws/frontend/controller/hyepin/DogMathingController.java
@@ -1,6 +1,5 @@
 package com.dogpaws.frontend.controller.hyepin;
 
-import com.dogpaws.frontend.global.ApiResponse;
 import com.dogpaws.frontend.service.ApiRequestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,30 +15,32 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/matching")
 @Slf4j
-public class MathingController {
+public class DogMathingController {
 
     private final ApiRequestService apiService;
 
     @GetMapping
     public String friendMatchingForm(Model model) {
 
-        // LikeDto -> Map<String, String> 변환
+        //Map<String, String> 변환
         Map<String, String> matchingFilterMap = Map.of(
                 "dogId", "1",
                 "matchType", "F"
         );
 
-
+        var filterResponse = apiService.fetchData("/api/matching", matchingFilterMap, true);
         var breedResponse = apiService.fetchData("/api/gubn/breed_code");
         var personalityResponse = apiService.fetchData("/api/gubn/dog_personal_code");
         var playResponse = apiService.fetchData("/api/gubn/dog_play_code");
         var dogTypeCodeResponse = apiService.fetchData("/api/gubn/dog_type_code");
 
+        var filter = filterResponse.getBody();
         var breedList = breedResponse.getBody();
         var personalityList = personalityResponse.getBody();
         var playList = playResponse.getBody();
         var dogTypeCodeList = dogTypeCodeResponse.getBody();
 
+        model.addAttribute("filter", filter);
         model.addAttribute("breedList", breedList);
         model.addAttribute("personalityList", personalityList);
         model.addAttribute("playList", playList);
