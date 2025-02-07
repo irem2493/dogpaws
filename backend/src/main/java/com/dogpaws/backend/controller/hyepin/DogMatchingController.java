dd9b8ac8a6b83bea7328dc613946388d03e8750c
DJDJ <gpqls9524@gmail.com>
2025-02-07 17:39:32 +0900

    0207 매칭 수정중

--- backend/src/main/java/com/dogpaws/backend/controller/hyepin/DogMatchingController.java
+++ backend/src/main/java/com/dogpaws/backend/controller/hyepin/DogMatchingController.java
@@ -1,6 +1,7 @@
 package com.dogpaws.backend.controller.hyepin;
 
-import com.dogpaws.backend.dto.hyepin.DogMatchDto;
+import com.dogpaws.backend.dto.hyepin.FilterDto;
+import com.dogpaws.backend.dto.hyepin.MatchDto;
 import com.dogpaws.backend.global.common.ApiResponse;
 import com.dogpaws.backend.service.hyepin.MatchingService;
 import lombok.RequiredArgsConstructor;
@@ -19,22 +20,22 @@ public class DogMatchingController {
     private final MatchingService matchingService;
 
     //매칭필터 가져오기
-    @GetMapping
-    public DogMatchDto getFilterBydogId(@RequestParam("dogId") int dogId,
-                                        @RequestParam("matchType") char matchType) throws IOException {
+    @GetMapping("/filter")
+    public FilterDto getFilterBydogId(@RequestParam("dogId") int dogId,
+                                      @RequestParam("matchType") char matchType) throws IOException {
         log.info("여기는 백 컨트롤러 getFilterBydogId / dogId 값: {}", dogId);
         log.info("여기는 백 컨트롤러 getFilterBydogId / matchType 값: {}", matchType);
-        DogMatchDto dogMatchDto = matchingService.getFilterBydogId(dogId, matchType);
-        log.info("여기는 백 컨트롤러 getFilterBydogId / dogMatchDto 값: {}", dogMatchDto);
-        return dogMatchDto;
+        FilterDto filterDto = matchingService.getFilterBydogId(dogId, matchType);
+        log.info("여기는 백 컨트롤러 getFilterBydogId / FilterDto 값: {}", filterDto);
+        return filterDto;
     }
 
     //매칭필터 등록 / 수정
     //match_type == "F / P" 확인하고, 정보 조회해서 null 값이면 등록, 아니면 수정 서비스로 보내기
-    @PostMapping
-    public ApiResponse<String> setMatchingFilter(@ModelAttribute DogMatchDto dogMatchDto) throws IOException {
-        log.info("여기는 백 컨트롤러 setMatchingFilter / dogMatchDto 값: {}", dogMatchDto);
-        int result = matchingService.setMatchingFilter(dogMatchDto);
+    @PostMapping("/filter")
+    public ApiResponse<String> setMatchingFilter(@ModelAttribute FilterDto filterDto) throws IOException {
+        log.info("여기는 백 컨트롤러 setMatchingFilter / FilterDto 값: {}", filterDto);
+        int result = matchingService.setMatchingFilter(filterDto);
         if (result == 1) {
             return new ApiResponse<>(ApiResponse.ApiStatus.SUCCESS, "필터 등록 성공");
         } else {
@@ -44,7 +45,7 @@ public class DogMatchingController {
 
     //매칭필터 삭제
     //match_type == "F / P" 확인하고, 정보 조회해서 null 값이면 저장된 필터 데이터가 없습니다. 아니면 삭제완료
-    @PostMapping("delete")
+    @PostMapping("/filter/delete")
     public ApiResponse<String> deleteFilter(@RequestParam int dogId,
                                                  @RequestParam char matchType) throws IOException {
         log.info("여기는 백 컨트롤러 deleteFilter / deleteFilter 값: {}", dogId, matchType);
@@ -55,6 +56,16 @@ public class DogMatchingController {
             return new ApiResponse<>(ApiResponse.ApiStatus.SUCCESS, "필터 삭제 실패");
         }
     }
+
+    //친구매칭 가져오기
+    @GetMapping
+    public List<MatchDto> getDogFriendMatchList(@RequestParam("dogId") int dogId) throws IOException {
+        log.info("여기는 백 컨트롤러 getDogFriendMatchList / dogId 값: {}", dogId);
+        List<MatchDto> matchList = matchingService.getDogFriendMatchList(dogId);
+        log.info("여기는 백 컨트롤러 getDogFriendMatchList / matchList 값: {}", matchList);
+        return matchList;
+    }
+
 
 
 
