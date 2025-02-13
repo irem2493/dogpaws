eb79d84334eeb92eb31586c0078740f53fad3346
JeyeonAhn <irem2493@naver.com>
2025-02-13 12:12:08 +0900

    2502013 토큰 문제 해결

--- backend/src/main/java/com/dogpaws/backend/repository/jpa/ajy/TokenRepository.java
+++ backend/src/main/java/com/dogpaws/backend/repository/jpa/ajy/TokenRepository.java
@@ -2,10 +2,12 @@ package com.dogpaws.backend.repository.jpa.ajy;
 
 import com.dogpaws.backend.entity.ajy.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     Optional<Token> findByUsername(String username);
+    @Transactional
     void deleteByUsername(String username);
 }
