55a444e3f94c47be9736931aaa1202f8a45a1200
안혜빈 <gpqls9524@naver.com>
2025-02-20 00:01:39 +0900

    250219 좋아요 리스트 백단 리스트 수정 완료

--- backend/src/main/java/com/dogpaws/backend/controller/hyepin/LikeListController.java
+++ backend/src/main/java/com/dogpaws/backend/controller/hyepin/LikeListController.java
@@ -3,6 +3,7 @@ package com.dogpaws.backend.controller.hyepin;
 import com.dogpaws.backend.dto.hyepin.FilterDto;
 import com.dogpaws.backend.dto.hyepin.LikeDogDto;
 import com.dogpaws.backend.repository.dao.hyepin.LikeDogDao;
+import com.dogpaws.backend.utils.StringUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +29,10 @@ public class LikeListController {
         log.info("여기는 백 컨트롤러 getLikeList / dogId 값: {}", myDogId);
         log.info("여기는 백 컨트롤러 getLikeList / likeCode 값: {}", likeCode);
         List<LikeDogDto> likeDogDtoList = likeDogDao.getLikeList(myDogId, likeCode);
+        for(LikeDogDto likeDog : likeDogDtoList){
+            likeDog.setDogPersonalGbnCdsList(StringUtil.splitToList(likeDog.getDogPersonalGbnCds()));
+            likeDog.setDogPlayGbnCdsList(StringUtil.splitToList(likeDog.getDogPlayGbnCds()));
+        }
         log.info("여기는 백 컨트롤러 getLikeList / likeDogDto 값: {}", likeDogDtoList);
         return likeDogDtoList;
     }
